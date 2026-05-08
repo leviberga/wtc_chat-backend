6d3e8e3b38cb553daae73ad670b85dbdeff3368d
Levi Bergamascki <lbergamascki@gmail.com>
2026-05-08 17:25:59 -0300

    feat: add security config, JWT, UserRepository and UserDetailsService

---
+++ src/main/java/com/wtc/chat_backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.wtc.chat_backend.security;
+
+import com.wtc.chat_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        com.wtc.chat_backend.model.User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + email));
+
+        return new User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
